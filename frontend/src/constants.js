8e0b9b34906b503985ba7c5e2348efefb68d6131
PRIYANSHkumar1 <85241815+PRIYANSHkumar1@users.noreply.github.com>
2025-02-07 01:21:58 +0530

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
-//export const BASE_URL =
-//process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://full-stack-project-backend-4qj7.onrender.com';
-export const BASE_URL = '';
+export const BASE_URL =
+process.env.NODE_ENV === 'development' ? 'http://localhost:10000' : 'https://full-stack-project-backend-4qj7.onrender.com';
+//export const BASE_URL = '';
 export const PRODUCTS_URL = '/api/v1/products';
 export const UPLOAD_URL = '/api/v1/upload';
 export const USERS_URL = '/api/v1/users';
